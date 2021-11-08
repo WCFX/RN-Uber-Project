791382062f61a685b69177e72cbccb0d2bac6ec5
Wagner Goulart <wcfx.dev@gmail.com>
2021-11-07 23:25:10 -0300

    chore: husky and lint-staged configured

---
+++ src/types/CardCarProps/index.ts
@@ -0,0 +1,11 @@
+interface CardCarProps {
+  key?: string;
+  brandname: string;
+  name: string;
+  period: string;
+  price: string;
+  type: 'gasoline' | 'energy';
+  car: string;
+}
+
+export default CardCarProps;
